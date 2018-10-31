ef028a4771d46786576bdf70801633cdd735ab02
Björn Kautler <Bjoern@Kautler.net>
2018-10-31 03:27:58 +0100

    Allow token to be read from given file
    This enables the proper usage of Docker secrets for the bot.

--- src/main/java/org/javacord/bot/Main.java
+++ src/main/java/org/javacord/bot/Main.java
@@ -2,10 +2,9 @@ package org.javacord.bot;
 
 import de.btobastian.sdcf4j.CommandHandler;
 import de.btobastian.sdcf4j.handler.JavacordHandler;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.util.logging.ExceptionLogger;
 import org.javacord.bot.commands.DocsCommand;
 import org.javacord.bot.commands.ExampleCommand;
 import org.javacord.bot.commands.GitHubCommand;
@@ -17,19 +16,43 @@ import org.javacord.bot.commands.Sdcf4jCommand;
 import org.javacord.bot.commands.SetupCommand;
 import org.javacord.bot.commands.WikiCommand;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
-    private static Logger logger = LogManager.getLogger(Main.class);
-
     /**
-     * The entrance point of the bot.
+     * The entry point of the bot.
      *
-     * @param args The first argument should be the bot's token. Every other argument gets ignored.
+     * @param args The bot requires exactly one argument, either a file with the token as content or the token directly.
+     *             If the argument is a relative file path, it is relative to the working directory.
+     * @throws IOException If there is an error when reading the token file.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Thread.setDefaultUncaughtExceptionHandler(ExceptionLogger.getUncaughtExceptionHandler());
+
+        if (args.length != 1) {
+            System.err.println("This bot requires exactly one argument, "
+                    + "either a file with the token as content or the token directly.\n"
+                    + "If the argument is a relative file path, it is relative to the working directory");
+            System.exit(1);
+        }
+
+        DiscordApiBuilder apiBuilder = new DiscordApiBuilder();
+
+        // Token
+        Path tokenFile = Paths.get(args[0]);
+        if (Files.isRegularFile(tokenFile)) {
+            apiBuilder.setToken(Files.newBufferedReader(tokenFile).readLine());
+        } else {
+            apiBuilder.setToken(args[0]);
+        }
+
         // Login
-        DiscordApi api = new DiscordApiBuilder()
-                .setToken(args[0])
+        DiscordApi api = apiBuilder
+                .setWaitForServersOnStartup(false)
                 .login().join();
 
         // Register commands
