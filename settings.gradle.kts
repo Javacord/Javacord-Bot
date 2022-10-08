303ecfbef2901023bcbf03cc0363e97c9d78ad51
Björn Kautler <Bjoern@Kautler.net>
2022-10-08 21:58:53 +0200

    Align log4j module versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "javacord-bot"
+
+dependencyResolutionManagement {
+    components {
+        all {
+            // align log4j module versions until log4j eventually publishes proper alignment
+            if (id.group == "org.apache.logging.log4j") {
+                belongsTo("org.apache.logging.log4j:log4j-bom:${id.version}", false)
+            }
+        }
+    }
+}
