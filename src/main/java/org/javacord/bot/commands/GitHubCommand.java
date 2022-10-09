6d5436c95220721e8fd90ea674de9dc717aa121e
Björn Kautler <Bjoern@Kautler.net>
2022-10-09 20:25:06 +0200

    Migrate !github to /github (#50)

--- src/main/java/org/javacord/bot/commands/GitHubCommand.java
+++ src/main/java/org/javacord/bot/commands/GitHubCommand.java
@@ -6,34 +6,30 @@ import net.kautler.command.api.CommandContext;
 import net.kautler.command.api.annotation.Alias;
 import net.kautler.command.api.annotation.Asynchronous;
 import net.kautler.command.api.annotation.Description;
-import net.kautler.command.api.parameter.Parameters;
 import org.apache.logging.log4j.Logger;
-import org.javacord.api.entity.channel.TextChannel;
-import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.interaction.SlashCommandInteraction;
 import org.javacord.bot.Constants;
-import org.javacord.bot.listeners.CommandCleanupListener;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * The !github command which is used to link to Javacord related GitHub repositories.
+ * The /github command which is used to link to Javacord related GitHub repositories.
  */
 @ApplicationScoped
 @Alias("github")
 @Description("Shows links to the most important GitHub pages")
 @Asynchronous
-public class GitHubCommand extends BaseTextCommand {
+public class GitHubCommand extends BaseSlashCommand {
     @Inject
     Logger logger;
 
     /**
-     * Executes the {@code !github} command.
+     * Executes the {@code /github} command.
      */
     @Override
-    protected void doExecute(CommandContext<? extends Message> commandContext, Message message,
-                             TextChannel channel, Parameters<String> parameters) {
+    public void execute(CommandContext<? extends SlashCommandInteraction> commandContext) {
         try (InputStream javacord3Icon = getClass().getResourceAsStream("/javacord3_icon.png")) {
             EmbedBuilder embed = new EmbedBuilder()
                     .addField("Javacord", "https://github.com/Javacord/Javacord")
@@ -41,8 +37,7 @@ public class GitHubCommand extends BaseTextCommand {
                     .addField("James", "https://github.com/Javacord/Javacord-Bot")
                     .setThumbnail(javacord3Icon)
                     .setColor(Constants.JAVACORD_ORANGE);
-            CommandCleanupListener.insertResponseTracker(embed, message.getId());
-            channel.sendMessage(embed).join();
+            sendResponse(commandContext.getMessage(), embed).join();
         } catch (IOException e) {
             logger
                     .atError()
