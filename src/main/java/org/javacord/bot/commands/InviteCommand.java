aba61fb8fc64d2a3e36bbd16389f27469267fed8
Björn Kautler <Bjoern@Kautler.net>
2022-10-09 22:51:18 +0200

    Migrate !invite to /invite (#50)

--- src/main/java/org/javacord/bot/commands/InviteCommand.java
+++ src/main/java/org/javacord/bot/commands/InviteCommand.java
@@ -5,41 +5,35 @@ import jakarta.inject.Inject;
 import net.kautler.command.api.CommandContext;
 import net.kautler.command.api.annotation.Asynchronous;
 import net.kautler.command.api.annotation.Description;
-import net.kautler.command.api.parameter.Parameters;
 import org.apache.logging.log4j.Logger;
-import org.javacord.api.entity.channel.TextChannel;
-import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.interaction.SlashCommandInteraction;
 import org.javacord.bot.Constants;
-import org.javacord.bot.listeners.CommandCleanupListener;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * The !invite command which is used to get an invite link to the Javacord Discord server.
+ * The /invite command which is used to get an invite link to the Javacord Discord server.
  */
 @ApplicationScoped
 @Description("Shows the invite link to the Javacord server")
 @Asynchronous
-public class InviteCommand extends BaseTextCommand {
+public class InviteCommand extends BaseSlashCommand {
     @Inject
     Logger logger;
 
     /**
-     * Executes the {@code !invite} command.
+     * Executes the {@code /invite} command.
      */
     @Override
-    protected void doExecute(CommandContext<? extends Message> commandContext, Message message,
-                             TextChannel channel, Parameters<String> parameters) {
+    public void execute(CommandContext<? extends SlashCommandInteraction> commandContext) {
         try (InputStream javacord3Icon = getClass().getResourceAsStream("/javacord3_icon.png")) {
             EmbedBuilder embed = new EmbedBuilder()
                     .setThumbnail(javacord3Icon)
                     .setColor(Constants.JAVACORD_ORANGE)
                     .addField("Invite Link", "https://discord.gg/javacord");
-
-            CommandCleanupListener.insertResponseTracker(embed, message.getId());
-            channel.sendMessage(embed).join();
+            sendResponse(commandContext.getMessage(), embed).join();
         } catch (IOException ioe) {
             logger
                     .atError()
