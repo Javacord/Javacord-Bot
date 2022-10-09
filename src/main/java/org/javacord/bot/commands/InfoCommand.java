cc6b67cb729e5004254c60ab1354ca40fe813053
Björn Kautler <Bjoern@Kautler.net>
2022-10-09 20:43:49 +0200

    Migrate !info to /info (#50)

--- src/main/java/org/javacord/bot/commands/InfoCommand.java
+++ src/main/java/org/javacord/bot/commands/InfoCommand.java
@@ -6,34 +6,30 @@ import net.kautler.command.api.CommandContext;
 import net.kautler.command.api.Version;
 import net.kautler.command.api.annotation.Asynchronous;
 import net.kautler.command.api.annotation.Description;
-import net.kautler.command.api.parameter.Parameters;
-import org.javacord.api.DiscordApi;
 import org.javacord.api.Javacord;
-import org.javacord.api.entity.channel.TextChannel;
-import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.user.User;
+import org.javacord.api.interaction.SlashCommandInteraction;
 import org.javacord.bot.Constants;
-import org.javacord.bot.listeners.CommandCleanupListener;
 
 @ApplicationScoped
 @Description("Shows information about this bot")
 @Asynchronous
-public class InfoCommand extends BaseTextCommand {
+public class InfoCommand extends BaseSlashCommand {
     @Inject
     Version commandFrameworkVersion;
 
     /**
-     * Executes the {@code !info} command.
+     * Executes the {@code /info} command.
      */
     @Override
-    protected void doExecute(CommandContext<? extends Message> commandContext, Message message,
-                             TextChannel channel, Parameters<String> parameters) {
-        DiscordApi api = channel.getApi();
+    public void execute(CommandContext<? extends SlashCommandInteraction> commandContext) {
+        User yourself = commandContext.getMessage().getApi().getYourself();
 
         EmbedBuilder embed = new EmbedBuilder()
                 .setColor(Constants.JAVACORD_ORANGE)
-                .setTitle(api.getYourself().getName() + " - Official Javacord Bot")
-                .setThumbnail(api.getYourself().getAvatar())
+                .setTitle(yourself.getName() + " - Official Javacord Bot")
+                .setThumbnail(yourself.getAvatar())
                 .setDescription("The official bot for the Javacord Server\n\n"
                         + "Powered by Javacord and command-framework")
                 .addInlineField("GitHub", "https://github.com/Javacord/Javacord-Bot")
@@ -43,8 +39,6 @@ public class InfoCommand extends BaseTextCommand {
                 .addInlineField("command-framework Version", String.format(
                         "[%s](https://github.com/Vampire/command-framework/releases/tag/v%s)",
                         commandFrameworkVersion.getDisplayVersion(), commandFrameworkVersion.getVersion()));
-
-        CommandCleanupListener.insertResponseTracker(embed, message.getId());
-        channel.sendMessage(embed).join();
+        sendResponse(commandContext.getMessage(), embed).join();
     }
 }
